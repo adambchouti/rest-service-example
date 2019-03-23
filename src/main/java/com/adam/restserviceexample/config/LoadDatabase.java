b52a941efe6e31420104e417a1db99fb80d8076a
Adam Bchouti <adambchouti93@gmail.com>
2019-03-23 17:57:52 -0400

    Changed the test data loading at runtime with new fields

--- src/main/java/com/adam/restserviceexample/config/LoadDatabase.java
+++ src/main/java/com/adam/restserviceexample/config/LoadDatabase.java
@@ -18,8 +18,8 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository employeeRepository) {
         return args -> {
-            log.info("Preloading: " + employeeRepository.save(new Employee("Bilbo Baggins", "burglar")));
-            log.info("Preloading: " + employeeRepository.save(new Employee("Frodo Baggins", "thief")));
+            log.info("Preloading: " + employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading: " + employeeRepository.save(new Employee("Frodo", "Baggins", "thief")));
         };
     }
 }
